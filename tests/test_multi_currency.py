d6719155acd4afee7c2d615922588af8d1d4764c
Matt Lebrun <matt@lebrun.org>
2025-11-03 16:35:34 +0800

    chapter-11 - Refactor - Deleted redundant Franc multiplication test

--- tests/test_multi_currency.py
+++ tests/test_multi_currency.py
@@ -14,13 +14,6 @@ def test_equality() -> None:
     assert not Money.franc(5).equals(Money.dollar(5))
 
 
-def test_franc_multiplication() -> None:
-    five:Money = Money.franc(5)
-
-    assert five.times(2) == Money.franc(10)
-    assert five.times(3) == Money.franc(15)
-
-
 def test_currency() -> None:
     assert "USD" == Money.dollar(1).currency()
     assert "CHF" == Money.franc(1).currency()
