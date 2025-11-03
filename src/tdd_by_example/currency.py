2b46379b2e9f328c8ff1373adc3918b142240a8d
Matt Lebrun <matt@lebrun.org>
2025-11-03 15:42:52 +0800

    chapter-10 - Changed implementation of `times` on Dollar and Franc to no use the factory

--- src/tdd_by_example/currency.py
+++ src/tdd_by_example/currency.py
@@ -43,7 +43,7 @@ class Dollar(Money):
         super().__init__(amount, currency)
 
     def times(self, multiplier: int) -> Money:
-        return Money.dollar(self._amount * multiplier)
+        return Dollar(self._amount * multiplier, "USD")
 
 
 class Franc(Money):
@@ -52,4 +52,4 @@ class Franc(Money):
         super().__init__(amount, currency)
 
     def times(self, multiplier: int) -> Money:
-        return Money.franc(self._amount * multiplier)
+        return Franc(self._amount * multiplier, "CHF")
