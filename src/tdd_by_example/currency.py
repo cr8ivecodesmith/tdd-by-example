04644e8c8149e1f52edd9c02c1ce8579f78bcafc
Matt Lebrun <matt@lebrun.org>
2025-11-03 14:00:46 +0800

    Used annotations future to improve type hint handling

--- src/tdd_by_example/currency.py
+++ src/tdd_by_example/currency.py
@@ -1,4 +1,6 @@
 """Currency module defining different currency classes."""
+from __future__ import annotations
+
 from abc import ABC
 
 
@@ -11,23 +13,23 @@ class Money(ABC):
         return self._amount
 
     @staticmethod
-    def dollar(amount: int) -> "Dollar":
+    def dollar(amount: int) -> Dollar:
         return Dollar(amount)
 
     @staticmethod
-    def franc(amount: int) -> "Franc":
+    def franc(amount: int) -> Franc:
         return Franc(amount)
 
     def __init__(self, amount: int) -> None:
         self._amount = amount
 
-    def equals(self, other: "Money") -> bool:
+    def equals(self, other: Money) -> bool:
         return (
             self.amount == other.amount
             and isinstance(other, type(self))
         )
 
-    def __eq__(self, other: "Money") -> bool:
+    def __eq__(self, other: Money) -> bool:
         return self.equals(other)
 
     def __repr__(self) -> str:  # pragma: no cover
@@ -36,11 +38,11 @@ class Money(ABC):
 
 class Dollar(Money):
 
-    def times(self, multiplier: int) -> "Money":
+    def times(self, multiplier: int) -> Money:
         return Dollar(self._amount * multiplier)
 
 
 class Franc(Money):
 
-    def times(self, multiplier: int) -> "Money":
+    def times(self, multiplier: int) -> Money:
         return Franc(self._amount * multiplier)
